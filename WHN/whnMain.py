73e840e36ef2a0941f0d5bfd82b3fd0806172e97
John Bonds III <johnwb0007@gmail.com>
2021-09-28 18:30:32 -0400

    complete v1.0 must have even number of players

--- WHN/whnMain.py
+++ WHN/whnMain.py
@@ -1,73 +1,99 @@
 import re, json, random
 from random import choice
 class Player:
-    def __init__(self, phoneNum):
-        self.phoneNum = phoneNum
+    def __init__(self, name):
+        self.name = name
         self.character = ''
         self.barList = []
+        self.jsonIndex = 0
 
-
-
-
-def game(player1, player2):
-    print()
-    print('player1 is % s' % player1.phoneNum)
-    print('player2 is % s' % player2.phoneNum)
-
+def game(player1, player2, charactersUsed):
     charJSON = json.load(open("characters.json"))
 
-    randNum = random.sample(range(0,len(charJSON) - 1), 2)
+    if len(range(0,len(charJSON) - 1)) - len(charactersUsed) != 0:
+        print()
+        print('player1 is % s' % player1.name)
+        print('player2 is % s\n\n' % player2.name)
 
-    player1.character = charJSON["charName"][randNum[0]]
+        characterInUse = list(set(range(0,len(charJSON) - 1)) - set(charactersUsed))
 
-    player2.character = charJSON["charName"][randNum[1]]
+        randNum = random.sample(characterInUse, 2)
 
-    player1.barList = charJSON[player1.character]
+        player1.jsonIndex, player2.jsonIndex = randNum[0], randNum[1]
 
-    player2.barList = charJSON[player2.character]
+        # print("character total: %s" % range(0,len(charJSON) - 1))
+        # print("charactersUsed: %s" % charactersUsed)
+        # print("characterInUse: %s" % characterInUse)
+        # print("randNum: %s" % randNum)
+        # print("randNum1: %s" % player1.jsonIndex)
+        # print("randNum2: %s" % player2.jsonIndex)
 
-    print(player1.barList)
 
-    print(player2.barList)
+        player1.character = charJSON["charName"][player1.jsonIndex]
 
-    while True:
-        print('stop')
-        x = input()
+        # print("p1: %s" % player1.character)
+
+        player2.character = charJSON["charName"][player2.jsonIndex]
+
+        # print("p2: %s" % player2.character)
+
+        player1.barList = charJSON[player1.character]
+
+        player2.barList = charJSON[player2.character]
+
+        print(player1.character)
+        print(player1.barList)
+
+        print('\n\n')
+        print(player2.character)
+        print(player2.barList)
+
+        charactersUsed.append(player1.jsonIndex)
+        charactersUsed.append(player2.jsonIndex)
+    else:
+        print('Out of character :(')
+        print('Add more characters for $4.99')
+        exit()
 
 def main():
     inputTxt = ''
-
-    phoneNumList = []
+    charactersUsed = []
+    nameList = []
 
     print('Enter phone numbers on players: ')
     print('Hit Enter start (s) when done\n')
 
-    print('Player {}'.format(len(phoneNumList) + 1))
-    print('Phone #: ', end='')
+    print('Player {}'.format(len(nameList) + 1))
+    print('Name: ', end='')
 
     #players input phone numbers
     while inputTxt != 's':
         inputTxt = input()
 
-        if inputTxt in phoneNumList:
+        if inputTxt in nameList:
             print('Number already registered\n')
-            print('Player {}'.format(len(phoneNumList) + 1))
+            print('Player {}'.format(len(nameList) + 1))
             print('Phone #: ', end='')
 
         elif inputTxt != 's':
-            if bool(re.match("[0-9]{10}", inputTxt)):
-                phoneNumList.append(inputTxt)
-                print('Entered.....\n\n')
-            else :
-                print('Input was not a phone number\n')
+            #Future phone number implementation
+            # if bool(re.match("[0-9]{10}", inputTxt)):
+            #     phoneNumList.append(inputTxt)
+            #     print('Entered.....\n\n')
+            # else :
+            #     print('Input was not a phone number\n')
 
-            print('Player {}'.format(len(phoneNumList) + 1))
-            print('Phone #: ', end='')
+            nameList.append(inputTxt)
+            print('Player {}'.format(len(nameList) + 1))
+            print('Name: ', end='')
 
-    for index, pNum in enumerate(phoneNumList):
-        if index % 2 == 0:
-            player1, player2 = Player(phoneNumList[index]), Player(phoneNumList[index+1])
-            game(player1, player2)
+    while inputTxt != 'd':
+        for index, pNum in enumerate(nameList):
+            if index % 2 == 0:
+                player1, player2 = Player(nameList[index]), Player(nameList[index+1])
+                game(player1, player2, charactersUsed)
+                print('\nEnter d for done else hit enter for next round')
+                inputTxt = input()
 
 if __name__ == "__main__":
     main()
